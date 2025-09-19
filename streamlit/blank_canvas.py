ff4cff6184522b246b5b210348c1cdbd2c64901d
Viktoria0720 <viyav0720@nmbu.no>
2025-09-19 16:27:44 +0200

    assignment added

--- streamlit/blank_canvas.py
+++ streamlit/blank_canvas.py
@@ -9,5 +9,5 @@ if st.button("Press me!"):
     st.write("You pressed the button!")
 
 # Add a slider with a range from 0 to 100 in increments of 2, starting at 50
-slider_num = st.slider("Select a value", 0, 100, value=50, step=2)
+slider_num = st.slider("Select a value", 0, 100, value=40, step=2)
 st.write("Slider value:", slider_num)
