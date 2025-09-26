f9393592d1f033d2aa2de32561b31e8f4e4860aa
Viktoria0720 <viyav0720@nmbu.no>
2025-09-26 13:58:11 +0200

    lib problem

--- ASSIGNMENTS/PART1/streamlit_app_1/app.py
+++ ASSIGNMENTS/PART1/streamlit_app_1/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from streamlit.elements import line_chart_column  
+#from streamlit.elements import line_chart_column  -- not able to import
 
 # -------------------------------
 # CACHE THE DATA LOADING FUNCTION
