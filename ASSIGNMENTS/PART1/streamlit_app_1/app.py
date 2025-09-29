75cbdfe379afc2dde36747ce0c21d3ff400d992c
Viktoria0720 <viyav0720@nmbu.no>
2025-09-29 17:32:50 +0200

    test

--- ASSIGNMENTS/PART1/streamlit_app_1/app.py
+++ ASSIGNMENTS/PART1/streamlit_app_1/app.py
@@ -88,7 +88,7 @@ elif page == "Data Table":
         col_left, col_right = st.columns([1, 3])
         col_left.write(col_name)
         # Mini sparkline chart
-        col_right.line_chart(df_first_month[[col_name]], height=50)
+        col_right.line_chart(df_first_month[[col_name]], height=200)
 # -------------------------------
 # PAGE 3: PLOTS + FILTERS
 # -------------------------------
